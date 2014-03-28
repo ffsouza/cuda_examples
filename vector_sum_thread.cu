15d1e8912d7f81ebcaa27221ebc517294a10386b
paulojamorim <paulojamorim@gmail.com>
2014-03-27 21:01:33 -0300

    finish vector_sum_thread

--- vector_sum_thread.cu
+++ vector_sum_thread.cu
@@ -6,8 +6,11 @@ __global__ void add(int *a, int *b, int *c)
 {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     
-    if(tid < N)
+    while(tid < N)
+    {
         c[tid] = a[tid] + b[tid];
+        tid += blockDim.x * gridDim.x;
+    }
 }
 
 
