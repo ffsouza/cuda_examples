7f3970410606f0ba47355df06816bb05dfdf9a2f
paulojamorim <paulojamorim@gmail.com>
2014-03-30 13:04:08 -0300

    Added shared memory example

---
+++ dot_product_shared_memory.cu
@@ -0,0 +1,21 @@
+#define imin(a,b)(a<b?a:b)
+
+const int N = 33 * 1024;
+const int threadsPerBlock = 256;
+
+__global__ void dot(float *a, float *b, float *c)
+{
+    __shared__ float cache[threadsPerBlock];
+
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int cache_index = threadIdx.x;
+
+    float temp = 0;
+    while(tid < N)
+    {
+        temp += a[tid] * b[tid];
+        tid += blockDim.x * gridDim.x;
+    }
+        
+    cache[cache_index] = temp;
+}
